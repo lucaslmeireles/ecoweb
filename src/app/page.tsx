c527244f7d893282981e724271131a0f5d4b8653
lucaslmeireles <lluciomeireles@gmail.com>
2023-06-21 00:43:29 +0000

    i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='bg-slate-400 rounded-md h-10 w-auto align-middle'>
+      <div className='bg-slate-400 rounded-md h-16 w-auto align-middle text-center'>
         <p className='text-center font-semibold text-lg'>
           Ola mundo
         </p>
