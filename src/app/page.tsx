483656b2f2a197f29478e5eb0636c04c3f6e02a4
lucaslmeireles <lluciomeireles@gmail.com>
2023-06-20 21:42:34 -0300

    testing designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='bg-slate-400 rounded-md h-10 w-auto align-middle'>
-        <p className='text-center font-semibold text-lg'>
+      <div className='bg-slate-400 rounded-md align-middle w-96 h-56 flex-col relative'>
+        <p className='font-semibold text-lg  text-center absolute ml-2 '>
           Ola mundo
         </p>
+        <div className=' bg-slate-300 rounded-e-md w-96 h-10 absolute bottom-0 hover:bg-slate-100 border border-s-slate-300'>
+
+        </div>
       </div>
     </main>
   )
