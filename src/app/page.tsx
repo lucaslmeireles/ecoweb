dc4059a578e46d055580f040ee8e4da70832e975
lucaslmeireles <lluciomeireles@gmail.com>
2023-06-22 19:07:14 +0000

    componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    
-    <main className="flex min-h-screen justify-between p-16">
-      <div className=' flex flex-col w-full'>
+    <main className="flex min-h-screen ">
+      <div className=' flex flex-col w-full p-16'>
       <div className=' flex flex-col w-full mb-8'>
         <h2 className='text-2xl font-semibold pb-6'>Popular</h2>
-        <div className='snap-x scroll-smooth flex justify-around'>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        </div>
       </div>
-      <div className=' flex flex-col w-full mb-8'>
-        <h2 className='text-2xl font-semibold pb-6'>Popular</h2>
-        <div className='grid-cols-4 grid col-span-4 gap-8 grid-flow-row'>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        </div>
-      </div>
+      
       <div className=' flex flex-col w-full mb-8'>
         <h2 className='text-2xl font-semibold pb-6'>Popular</h2>
         <div className='grid-cols-4 grid col-span-4 gap-8 grid-flow-row'>
@@ -45,7 +29,7 @@ export default function Home() {
         </div>
       </div>
     </div>
-    <div className='ml-8 flex flex-col'>
+    <div className='ml-8 flex flex-col pr-16 pt-16'>
       <div className="w-[240px] h-[389px] relative ">
           <div className="w-60 h-[389px]  bg-zinc-300 rounded-3xl" />
           <div className="w-[123px] h-[123px] left-[58px] top-[29px] absolute bg-red-600 rounded-full" />
@@ -53,7 +37,7 @@ export default function Home() {
           <div className="w-[69px] h-[45px] left-[31px] top-[278px] absolute text-black text-[15px] font-medium">Followers</div>
           <div className="w-[69px] h-[45px] left-[133px] top-[278px] absolute text-black text-[15px] font-medium">Followers<br/></div>
       </div>
-      <div className="w-[240px] h-[117px] relative mt-5">
+      <div className="w-[240px] h-[117px] relative mt-5 pr-16 pt-16 ">
         <div className="w-[240px] h-[117px] left-[0px] top-[0px] absolute bg-sky-500 rounded-[25px]" />
         <div className="w-[99px] h-[28px] left-[107px] top-[77px] absolute bg-green-500 rounded-[30px]" />
         <div className="left-[119px] top-[82px] absolute text-white text-[15px] font-medium">IQ 45 BOM</div>
