1473c33c2b20cac5b9e869f8486d8cf0417ab7b6
lucaslmeireles <lluciomeireles@gmail.com>
2023-07-20 20:19:50 +0000

    create post

--- src/app/posts/create/myEditor.tsx
+++ src/app/posts/create/myEditor.tsx
@@ -1,18 +1,38 @@
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import dynamic from 'next/dynamic';
+import { EditorState } from 'draft-js';
+import type {ComponentType} from 'react';
+
+type EditorWyiwgt = {
+    editorState: EditorState;
+    onEditorStateChange: (EditorState) => EditorState;
+    toolbarClassName: string;
+    editorClassName: string;
+}
+
+//tentar resolver esse BO
+const Editor: EditorWyiwgt = dynamic(() => import('react-draft-wysiwyg').then(module => module.Editor),{
+    ssr: false
+})
 
 
 function MyEditor() {
-    const [editorState, setEditorState] = useState(
-        EditorState.createEmpty()
-      );
-    const handleChange = (state) => {
-        setEditorState(state)
+    const [editorState, setEditorState] = useState(EditorState.createEmpty())
+    const onEditorStateChange = (editorState : EditorState) => {
+        console.log(editorState)
+        setEditorState(editorState)
     }
     return (
-    <Editor editorState={editorState} onChange={handleChange} />
+        <div className='bg-slate-50 py-4'>
+            <Editor 
+            editorState = {editorState}
+            toolbarClassName='flex sticky top-0 z-50 justify-center mx-auto'
+            onEditorStateChange = {onEditorStateChange}
+            editorClassName = 'bg-white shadow-lg max-w-5xl'
+            />
+        </div>
     )
 }
 
