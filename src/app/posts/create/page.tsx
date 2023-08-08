8f91a622263f207cb3597479db6f158d4a41b009
lucaslmeireles <lluciomeireles@gmail.com>
2023-08-08 13:14:18 +0000

    bastante coisa resolvida

--- src/app/posts/create/page.tsx
+++ src/app/posts/create/page.tsx
@@ -6,36 +6,33 @@ import { EditorState, convertToRaw } from "draft-js";
 import { useSession } from "next-auth/react";
 import draftToHtml from 'draftjs-to-html';
 import { uploadImageCloudnary } from "@/app/data/cloudnaryService";
+import CreatableSelect from 'react-select/creatable';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
 
 function CreatePost() {
     const {data : session} = useSession()
     const [editorState, setEditorState] = useState(EditorState.createEmpty())
     const [modalVisible, setModalVisible] = useState(false)
-    const [choices, setChoices] = useState(['teste','test2'])
+    const [choices, setChoices] = useState([])
     const onEditorStateChange = (editorState : EditorState) => {
         setEditorState(editorState)
     }
-    const renderChoices = () => {
-        return choices.map((choice, index) => (
-        <>
-          <input
-            key={index}
-            type="checkbox"
-            name={`checkbox${index}`}
-            value={choice}
-          />
-          <label>{choice}</label>
-        </>
-        ));
-      };
-    
+    //https://www.cluemediator.com/how-to-get-selected-by-only-value-for-multi-select-in-react-select
+    const onSelectedTag = (e) => {
+        setChoices(Array.isArray(e) ? e.map(x => x.value.toLowerCase()) : [])
+        console.log(choices)
+    }
     const sendPost =  async (event: React.FormEvent) => {
         event.preventDefault()
         const title = event.target.title.value as string
         const small_text = event.target.small_text.value as string
-        const tags = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map((tag) => {
-            return tag.value;
-        })
+        const tags = choices
         const rawContentState = convertToRaw(editorState.getCurrentContent())
         const imgurl = await uploadImageCloudnary(event.target.img.files[0])
         console.log(imgurl)
@@ -66,11 +63,13 @@ function CreatePost() {
             body: JSON.stringify(body)
         })
         const reponse = await res.json()
-        console.log(reponse)
+        if (res.status == 200){
+            setModalVisible(true)
+        }
         
-        setModalVisible(false)
     }
     return (
+        <>
         <main className="mx-5 px-3 my-3 py-2">
             <h1 className="text-2xl text-neutral-900 font-semibold py-1">Create your post</h1>
             <small className="text-teal-600 text-base font-normal  flex algin-middle items-center">Share with the world <BiWorld className="mx-1"/></small>
@@ -80,26 +79,17 @@ function CreatePost() {
                     <input name="title" id="title" className="bg-slate-100 text-neutral-950 w-1/4 rounded-lg border-teal-400" type="text" />
                     <label>Description</label>
                     <input name="small_text" id="small_text" className="bg-slate-100 text-neutral-950 w-1/4 rounded-sm" type="text" />
-                    <label>Tags</label>
-                    <ul>
-                        {renderChoices()}
-                        {/*
-                        
-                        <li>
-                            Write a new one!
-                            <input type="text" name="plusone" id="plusone"></input>
-                        </li>
-                    
-    */}
-                    </ul>
                     <label>Share your post</label>
                     <MyEditor editorState={editorState} onEditorStateChange={onEditorStateChange}/>
+                    <label>Tags</label>
+                    <CreatableSelect  onChange={(choice) => onSelectedTag(choice)} isMulti options={options} />
                     <label>Cover photo</label>
                     <input className="bg-slate-100 text-neutral-950" type="file" name="img" id="img" />
                     <button type="submit" disabled={modalVisible} className="bg-teal-500 rounded-md w-1/4 h-10  text-neutral-950 self-center hover:bg-teal-900 hover:text-blue-50">Publish</button>
                 </form>
             </div>
         </main>
+        </>
     )
 }
 
